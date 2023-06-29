744fbc63f99a1e5cde1536997fc1ed32ace90d9d
Achille DAVID <davidachille18@gmail.com>
2023-06-29 22:54:19 +0200

    ajout de produit

--- sell_me_out/src/app/PAGES/new/new.component.ts
+++ sell_me_out/src/app/PAGES/new/new.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { ProductsService } from 'src/app/SERVICES/products.service';
 
 @Component({
   selector: 'app-new',
@@ -6,11 +8,21 @@ import { Component } from '@angular/core';
   styleUrls: ['./new.component.scss'],
 })
 export class NewComponent {
+  constructor(private product: ProductsService) {}
+
   back() {
     window.history.back();
   }
 
-  create() {
-    console.log('create');
+  create(form: NgForm) {
+    const product = {
+      name: form.value.name,
+      description: form.value.description,
+      price: form.value.price,
+      active: form.value.active,
+    };
+    this.product.create_product(product).subscribe((res) => {
+      console.log(res);
+    });
   }
 }
--- sell_me_out/src/app/SERVICES/products.service.ts
+++ sell_me_out/src/app/SERVICES/products.service.ts
@@ -1,12 +1,13 @@
 import { Injectable } from '@angular/core';
 import { BackService } from './back.service';
 import { Observable } from 'rxjs';
+import { TokenService } from './token.service';
 
 @Injectable({
   providedIn: 'root',
 })
 export class ProductsService {
-  constructor(private back: BackService) {}
+  constructor(private back: BackService, private token: TokenService) {}
 
   get_products(): Observable<any> {
     return this.back.get('products');
@@ -18,6 +19,17 @@ export class ProductsService {
 
   get_product(id: number): Observable<any> {
     return this.back.get(`product/${id}`);
+  }
+
+  create_product(product: any): Observable<any> {
+    const formData = new FormData();
+    formData.append('nom', product.name);
+    formData.append('description', product.description);
+    formData.append('prix', product.price.toString());
+    formData.append('visibility', product.active);
+    const id = this.token.getId();
+    formData.append('userId', id.toString());
+    return this.back.post_data('product', formData);
   }
 
   togge_visibility(id: number): Observable<any> {
